12c33773b9947e1a97f1060a8606f61c92f84834
derrickdrye <dryd95@hotmail.com>
2023-06-19 11:39:07 -0500

    began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace MethodsExercise
+﻿using Microsoft.VisualBasic;
+using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // ---------------------------------------Exercise 1-------------------------------------------------------
+            //Name: Derrick
+            //Favorite Color: Navy blue
+            //Favorite Animal: Birds
+            //Favorite Band: New Edition
+
+
+
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is a great color. It's so relaxing.  What's your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Verrry Nice!  Now, what band are you fond of?");
+            var band = Console.ReadLine();
+
         }
     }
 }
