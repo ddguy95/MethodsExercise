393a391c5c16de4d1b580f1cf05ead1b939b9f98
derrickdrye <dryd95@hotmail.com>
2023-06-19 12:04:50 -0500

    finished profile configuration

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -27,6 +27,16 @@ namespace MethodsExercise
             Console.WriteLine($"Verrry Nice!  Now, what band are you fond of?");
             var band = Console.ReadLine();
 
+            Console.WriteLine($"Thanks, {userName}! Here is your profile...");
+            Console.WriteLine("--------------------------------------------------------------");
+            
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Favorite Color: {color}");
+            Console.WriteLine($"Favorite Animal: {animal}");
+            Console.WriteLine($"Favorite Band: {band}");
+            
+
+
         }
     }
 }
